1d8a029e7e97f4b4c0e4ac59614c8de3c3061f8d
VijayKumar Thamina <tvijaykumar.software@gmail.com>
2026-09-07 20:37:46 +0530

    Second Changes

--- EcommerceAPI/obj/Debug/net10.0/EcommerceAPI.AssemblyInfo.cs
+++ EcommerceAPI/obj/Debug/net10.0/EcommerceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b76dde05fe8e2d8377f56c50e16ac8b3227c5682")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
